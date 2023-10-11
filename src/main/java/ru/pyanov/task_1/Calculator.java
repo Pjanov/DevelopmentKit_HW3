029e6257b830c1c3856895fb9c62c2ee14897e0d
pjanov <pjanov84@mail.ru>
2023-10-11 23:13:21 +0300

    Finish Task_1

--- src/main/java/ru/pyanov/task_1/Calculator.java
+++ src/main/java/ru/pyanov/task_1/Calculator.java
@@ -1,4 +1,28 @@
 package ru.pyanov.task_1;
 
+/**
+ * Написать класс Калькулятор (необобщенный), который содержит обобщенные статические
+ * методы: sum(), multiply(), divide(), subtract(). Параметры этих методов – два числа
+ * разного типа (но необязательно разного между собой), над которыми должна быть
+ * произведена операция.
+ */
 public class Calculator {
+    public static <T extends Number, U extends Number> double sum(T num1, U num2) {
+        return num1.doubleValue() + num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double multiply(T num1, U num2) {
+        return num1.doubleValue() * num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double divide(T num1, U num2) {
+        if (num2.doubleValue() == 0) {
+            throw new ArithmeticException("Деление на ноль не допускается!");
+        }
+        return num1.doubleValue() / num2.doubleValue();
+    }
+
+    public static <T extends Number, U extends Number> double subtract(T num1, U num2) {
+        return num1.doubleValue() - num2.doubleValue();
+    }
 }
--- src/main/java/ru/pyanov/task_1/Main.java
+++ src/main/java/ru/pyanov/task_1/Main.java
@@ -2,6 +2,19 @@ package ru.pyanov.task_1;
 
 public class Main {
     public static void main(String[] args) {
+        int num_1 = 4;
+        double num_2 = 5.5;
 
+        double sum = Calculator.sum(num_1, num_2);
+        System.out.println("Сумма: " + num_1 + " + " + num_2 + " = " + sum);
+
+        double multiply = Calculator.multiply(num_1, num_2);
+        System.out.println("Умножение: " + num_1 + " * " + num_2 + " = " + multiply);
+
+        double divide = Calculator.divide(num_1, num_2);
+        System.out.println("Деление: " + num_1 + " / " + num_2 + " = " + divide);
+
+        double subtract = Calculator.subtract(num_1, num_2);
+        System.out.println("Вычитание: " + num_1 + " - " + num_2 + " = " + subtract);
     }
 }
