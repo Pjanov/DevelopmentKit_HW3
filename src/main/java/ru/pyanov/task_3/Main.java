21dfc88fd52e005827722fda45fb945fd8c7a5a5
pjanov <pjanov84@mail.ru>
2023-10-11 23:50:33 +0300

    Finish Task_3

--- src/main/java/ru/pyanov/task_3/Main.java
+++ src/main/java/ru/pyanov/task_3/Main.java
@@ -1,4 +1,11 @@
 package ru.pyanov.task_3;
 
 public class Main {
+    public static void main(String[] args) {
+        Pair<String, Integer> pair1 = new Pair<>("Hello", 39);
+        System.out.println(pair1);
+
+        Pair<Double, String> pair2 = new Pair<>(3.14, "World");
+        System.out.println(pair2);
+    }
 }
--- src/main/java/ru/pyanov/task_3/Pair.java
+++ src/main/java/ru/pyanov/task_3/Pair.java
@@ -1,4 +1,30 @@
 package ru.pyanov.task_3;
 
-public class Pair {
+/**
+ * Напишите обобщенный класс Pair, который представляет собой пару значений разного типа.
+ * Класс должен иметь методы getFirst(), getSecond() для получения значений каждого из
+ * составляющих пары, а также переопределение метода toString(), возвращающее строковое
+ * представление пары.
+ */
+public class Pair<T, U> {
+    private final T first;
+    private final U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + first.toString() + ", " + second.toString() + "}";
+    }
 }
